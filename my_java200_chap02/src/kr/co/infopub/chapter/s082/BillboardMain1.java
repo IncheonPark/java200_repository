6b1cf6c21b773c85b15f73b6980f97aa32db727b
IncheonPark <dnehd8@gmail.com>
2021-01-26 13:11:07 +0900

    to s084. 26-Jan-21

---
+++ my_java200_chap02/src/kr/co/infopub/chapter/s082/BillboardMain1.java
@@ -0,0 +1,21 @@
+// 빌보드 차트 정보에서 사용하는 정수와 문자열
+
+package kr.co.infopub.chapter.s082;
+
+public class BillboardMain1 {
+	
+	public static void main(String[] args) {
+		
+		int rank = 1;
+		String song = "Despacito";
+		
+		// Data를 그릇에 담는다.
+		int lastweek = 1;
+		String imagesrc="https://www.billboard.com/images/pref_images/pref_images/q61808osztw.jpg";
+		String artist = "luis fonsi";
+		String sf = String.format("%d, %s, %d, %s, %s", rank, song, lastweek, imagesrc, artist);
+		System.out.println(sf);
+		
+	}
+
+}
---
+++ my_java200_chap02/src/kr/co/infopub/chapter/s084/BillboardMainObject.java
@@ -0,0 +1,40 @@
+//빌보드 차트를 객체에 저장하고 출력하기
+
+package kr.co.infopub.chapter.s084;
+
+import kr.co.infopub.chapter.s083.Billboard;
+
+public class BillboardMainObject {
+	
+	public static void main(String[] args) {
+		
+		Billboard b1 = new Billboard(1, "Despacito",1, "https://www.billboard.com/images/pref_images/q61808osztw.jpg", "luis fonsi");
+		Billboard b2 = new Billboard(2, "Despacito",2, "https://www.billboard.com/images/pref_images/q61808osztw.jpg", "luis fonsi");
+		Billboard b3 = new Billboard(3, "Despacito",3, "https://www.billboard.com/images/pref_images/q61808osztw.jpg", "luis fonsi");
+		showAbout(b1);
+		showAbout(b2);
+		showAbout(b3);
+		
+		Billboard[] b = new Billboard[3];
+		b[0] = b1;
+		b[1] = b2;
+		b[2] = b3;
+		showAbout(b[0], b[2]);
+		showAbout(b);
+		
+	}
+	
+	public static void showAbout(Billboard bb ) {
+		System.out.println("1----------------");
+		String sf = String.format("%d, %s, %d, %s, %s", bb.getRank(),bb.getSong(),bb.getLastweek(),bb.getImagesrc(),bb.getArtist());
+		System.out.println(sf);
+	}
+	
+	public static void showAbout(Billboard ... b) { // 가변 인수
+		System.out.println("2----------------");
+		for (Billboard bill : b) {
+			showAbout(bill);
+		}
+	}
+
+}

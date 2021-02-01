07280699880a411aa965176ab63216d4a62f0dea
park <park@58.232.121.77>
2021-02-01 14:15:33 +0900

    to s085. 01-Feb-21

--- my_java200_chap02/src/kr/co/infopub/chapter/s085/BillboardArrayMain.java
+++ my_java200_chap02/src/kr/co/infopub/chapter/s085/BillboardArrayMain.java
@@ -2,11 +2,15 @@
 
 package kr.co.infopub.chapter.s085;
 
+import java.util.ArrayList;
+
+import kr.co.infopub.chapter.s083.Billboard;
+
 public class BillboardArrayMain {
 	
 	public static void main(String[] args) {
 		
-		
+		ArrayList<Billboard> bills = new ArrayList<>();
 		
 	}
 
